d3fb15a7a2316b48b4075b4d2f55e9b16800db7c
Enrique Tezozomoc <merrovingo@gmail.com>
2014-02-05 23:46:03 -0600

    Scripts faltantes
    Scripts para instalar moodle y mysql

---
+++ moodle.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+# Instalacion de moodle y git
+# Debian 7
+clear
+echo "Script instalador de Moodle"
+echo "Esta ejecutando el Scrip como root?"
+echo "1 - Si"
+echo "2 - No"
+read respuesta
+case $respuesta in
+	1) echo "Instalando Git..."
+	sleep 2
+	apt-get install -y git
+	echo "Escriba el nombre para su curso de Moodle:"
+	read curso
+	mkdir /var/www/$curso
+	cd /var/www/$curso
+	echo "Clonando el repositorio de Git..."
+	sleep 2
+	git clone https://github.com/moodle/moodle.git
+	cd moodle
+	echo "Creando moodledata..."
+	mkdir /var/moodledata
+	chmod 777 /var/moodledata
+	echo "Instalacion terminada"
+	sleep 1
+	;;
+	2) echo "Necesita ejecutar el script como root"
+	sleep 2
+	;;
+	*) echo "Opcion no valida"
+	sleep 2
+	;;
+esac
---
+++ mysql.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+# Instalacion de mysql
+# Debian 7
+clear
+echo "Script instalador de mysql"
+echo "Esta ejecutando el Scrip como root?"
+echo "1 - Si"
+echo "2 - No"
+read respuesta
+case $respuesta in
+	1) echo "Ingrese su contraseña de root para mysql:"
+	read pass
+	echo "Instalando mysql server..."
+	sleep 1
+	apt-get install -q -y mysql-server
+	echo "A continuacion crearemos un usuario mysql para moodle"
+	echo "Ingrese el nombre de usuario:"
+	read usuariodb
+	echo "Ingrese una contraseña:"
+	read passuser
+	mysql -u root -p -e "CREATE USER $usuariodb@'localhost' IDENTIFIED BY '$passuser';"
+	echo "A continuacion crearemos una bd para moodle"
+	echo "Ingrese el nombre que desea para la bd:"
+	read nombrebd
+	mysql -u root -p -e "CREATE DATABASE $nombrebd CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
+	mysql -u root -p -e "GRANT ALL PRIVILEGES ON $nombrebd.* TO $usuariodb@'localhost';"
+	mysql -u root -p -e "FLUSH PRIVILEGES;"
+	mysqladmin -u root password $pass
+	clear
+	echo "Instalacion terminada"
+	echo "Resumen de la instalacion:"
+	echo "Su contraseña de root es: " $pass
+	echo "Su nombre de usuario es: " $usuariodb
+	echo "La contraseña para $usuariodb es: " $passuser
+	echo "La base de datos creada para moodle es: " $nombrebd
+	sleep 1
+	;;
+	2) echo "Necesita ejecutar el script como root"
+	sleep 2
+	;;
+	*) echo "Opcion no valida"
+	sleep 2
+	;;
+esac
