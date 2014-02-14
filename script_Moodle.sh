0f6b3983d8d298d04279b8f7dac07acb43887767
Enrique Tezozomoc <merrovingo@gmail.com>
2014-02-14 00:28:08 -0600

    Cambios Script
    Agregado el Virtual Host, errores en paso de variables y no funciona el
    sistema de autentificación

--- script_Moodle.sh
+++ script_Moodle.sh
@@ -1,17 +1,38 @@
-#/bin/bash
+#!/bin/sh
 # Script de instalacion de la plataforma Moodle
 # El siguiente script instalara: Apache, MySQL, Git y Moodle
 # Esta diseñado para funcionar con Debian 7 x86, x86_64
 # Autor : Enrique Tezozomoc Perez Campos
 # merrovingo@gmail.com
-############################################################################################################
+##########################################################################################################################
+##########################################################################################################################
 DEBIAN_FRONTEND=noninteractive
 clear
-############################################################################################################
+##########################################################################################################################
+##########################################################################################################################
 #		Definicion de variables		#
-############################################################################################################
+#		Sientase libre de modificar las siguientes variables segun sus necesidades particulares		#
+##########################################################################################################################
+##########################################################################################################################
+# 		Datos del virtualhost 				#
+# Nombre del sitio
+NOMBRE_SITIO="moodle"
+# Correo electronico del administrador del sitio
+APACHE_MAIL="curso@superserver.com"
+# Nombre del servidor
+APACHE_SERVER="superserver.com"
+# Nombre del archivo de log de errores
+APACHE_ERROR="moodle-error.log"
+# Nombre del archivo de log de acceso al sitio
+APACHE_ACCESS="moodle-acceso.log"
+###########################################################################################################################
+# 		Datos del curso de Moodle 			#
 # Nombre del curso para Moodle
 CURSO="diplomado_innovacion"
+# Rama Moodle a utilizar
+RAMA_MOODLE="MOODLE_26_STABLE"
+############################################################################################################################
+# 		Datos de configuracion de MySQL 	#
 # Contraseña de root para MySQL
 PASSWORD="moodle_secreto"
 # Nombre de la base de datos para Moodle
@@ -20,12 +41,31 @@ BASE_MOODLE="moodle"
 USR_MOODLE="usr_moodle"
 # Contraseña para el usuario de MySQL de Moodle
 PASSWORD_MOODLE="usuario_secreto"
-#############################################################################################################
+############################################################################################################################
+############################################################################################################################
 #		Definicion de funciones		#
-#############################################################################################################
+############################################################################################################################
+############################################################################################################################
 function script_apache(){
 	echo "Instalando Apache..."
 	apt-get install -y libapache2-mod-php5
+	echo "Configurando el Virtualhost..."
+	cd /etc/apache/sites-available && touch $NOMBRE_SITIO
+	echo "<virtualhost *.80>" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "ServerAdmin '$APACHE_MAIL'" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "ServerName '$APACHE_SERVER'" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "ServerSignature Off" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "DocumentRoot /var/www/'$CURSO'/" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "<Directory /var/www/'$CURSO'/moodle>" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "Options -Indexes FollowSymLinks MultiViews" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "AllowOverride ALL" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "Order Allow, Deny" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "Allow From All" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "</Directory>" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "LogLevel warn" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "ErrorLog ${APACHE_LOG_DIR}/'$APACHE_ERROR'" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "CustomLog ${APACHE_LOG_DIR}/'$APACHE_ACCESS' combined" >> /etc/apache/sites-available/$NOMBRE_SITIO
+	echo "</virtualhost>" >> /etc/apache/sites-available/$NOMBRE_SITIO
 	echo "Instalacion de Apache terminada"
 }
 function script_moodle(){
@@ -35,6 +75,9 @@ function script_moodle(){
 	mkdir /var/www/$CURSO
 	cd /var/www/$CURSO
 	git clone https://github.com/moodle/moodle.git
+	echo "Cambiando a la rama '$RAMA_MOODLE'"
+	git branch --track $RAMA_MOODLE origin/$RAMA_MOODLE
+	echo "Creando moodledata..."
 	mkdir /var/moodledata
 	chmod 777 /var/moodledata
 	echo "Instalacion de Moodle terminada"
@@ -61,12 +104,14 @@ function script_php(){
 	php5-intl php5-mcrypt php5-xsl php5-xmlrpc mcrypt libgd-tools
 	echo "Instalacion de PHP terminada"
 }
-#################################################################################################################
+###########################################################################################################################
+###########################################################################################################################
 #		Main						#
-#################################################################################################################
+###########################################################################################################################
+###########################################################################################################################
 echo "Script para la instalacion de Moodle"
 echo "El siguiente script necesita ser ejecutado como root"
-echo "Ingrese su contraseña:"
+echo "Por favor autentifiquese..."
 su
 if [[ '$USERNAME' = "root" ]]; then
 	script_apache
@@ -77,5 +122,5 @@ if [[ '$USERNAME' = "root" ]]; then
 else
 	echo "Para realizar la instalacion de Moodle se necesitan realizar operaciones"
 	echo "que requieren permisos de root, por favor intentelo nuevamente"
-	bash script_Moodle.sh
+	sh script_Moodle.sh
 fi
