0b99e9f3c7bab027d9e42feda8724338d5927406
Enrique Tezozomoc <merrovingo@gmail.com>
2015-02-01 00:20:07 -0600

    Proyecto Version 1 del script Moodle

---
+++ script_Moodle_V1.sh
@@ -0,0 +1,6 @@
+#### Script de instalacion para Moodle 2.X 	####
+#### Autor: Enrique Tezozomoc Perez Campos 	####
+#### http://error066.blogspot.com 		####
+#### UNAM 2015 					####
+#### Febrero 2014				####
+####################################################
