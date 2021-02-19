51c02ffd0f7975d2afab281093854b30e253ae0c
Enrique Tezozomoc <merrovingo@gmail.com>
2021-02-19 11:10:07 -0600

    Update script_Moodle_V3.sh
    agregado postfix

--- script_Moodle_V3.sh
+++ script_Moodle_V3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt -y install php7.3 php7.3-{mysql,curl,gd,gmp,intl,xsl,xmlrpc,soap,zip} memcached
-apt -y install mariadb-server apache2 git net-tools dnsutils acl htop curl
+apt -y install mariadb-server apache2 git net-tools dnsutils acl htop curl postfix mailutils
 a2enmod ssl rewrite
 systemctl restart apache2.service
 rm /var/www/html/index.html
