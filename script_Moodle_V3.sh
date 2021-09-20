02c9fec2755843b2449ac24b91b63251f5fcd29d
Enrique Tezozomoc <merrovingo@gmail.com>
2021-09-20 10:31:33 -0500

    Update script_Moodle_V3.sh

--- script_Moodle_V3.sh
+++ script_Moodle_V3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt -y install php7.3 php7.3-{mysql,curl,gd,gmp,intl,xsl,xmlrpc,soap,zip} memcached
+DEBIAN_FRONTEND=noninteractive
+apt -y install php7.3 php7.3-{mysql,curl,gd,gmp,intl,xsl,xmlrpc,soap,zip,mbstring} memcached
 apt -y install mariadb-server apache2 git net-tools dnsutils acl htop curl postfix mailutils
 a2enmod ssl rewrite
 systemctl restart apache2.service
